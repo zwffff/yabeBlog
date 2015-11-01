d56566cc3c3bbfe62cd577c02ae3e541e68df6b2
zwffff <zwf98000@163.com>
2015-11-01 18:21:13 +0800

    Add models and Tests

--- app/models/Comment.java
+++ app/models/Comment.java
@@ -1,8 +1,31 @@
 package models;
 
+import play.db.jpa.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import java.util.Date;
+
 /**
  * Author: DarrenZeng
  * Date: 2015-11-01
  */
-public class Comment {
+@Entity
+public class Comment extends Model {
+    public String author;
+    public Date postedAt;
+
+    @Lob
+    public String content;
+
+    @ManyToOne
+    public Post post;
+
+    public Comment(Post post, String author, String content) {
+        this.post = post;
+        this.author = author;
+        this.content = content;
+        this.postedAt = new Date();
+    }
 }
--- app/models/Post.java
+++ app/models/Post.java
@@ -1,8 +1,42 @@
 package models;
 
+import play.db.jpa.Model;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 /**
  * Author: DarrenZeng
  * Date: 2015-11-01
  */
-public class Post {
+@Entity
+public class Post extends Model {
+    public String title;
+    public Date postedAt;
+
+    @Lob
+    public String content;
+
+    @ManyToOne
+    public User author;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
+    public List<Comment> comments;
+
+    public Post(User author, String title, String content) {
+        this.comments = new ArrayList<Comment>();
+        this.author = author;
+        this.title = title;
+        this.content = content;
+        this.postedAt = new Date();
+    }
+
+    public Post addComment(String author, String content) {
+        Comment newComment = new Comment(this, author, content);
+        this.comments.add(newComment);
+        this.save();
+        return this;
+    }
 }
